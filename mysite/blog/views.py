e38eab17449105378985e17795ecab530e8f74c8
Nadhif Ilyasa <nadhif909@gmail.com>
2019-09-14 13:48:22 +0700

    menambahkan models ke dalam template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 import pandas as pa
 import matplotlib as plot
 
@@ -15,6 +17,7 @@ def about(request):
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
     
